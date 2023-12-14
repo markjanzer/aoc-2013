cf16fd2114c0c315089a888c55c0d993fd46ac62
Mark Janzer <mark.janzer@gmail.com>
2023-12-14 01:18:52 -0800

    Add tests and get first_and_last to use slices

--- 1-trebuchet.go
+++ 1-trebuchet.go
@@ -8,31 +8,40 @@ import (
 )
 
 func main() {
+	// old value 11, new value 29
+	test_calibration_value("two1nine", 11)
+	// old value 24, new value 14
+	test_calibration_value("zoneight234", 24)
+	// old value 22, new value 13
+	test_calibration_value("abcone2threexyz", 22)
+
+	return
+
 	// Read text from https://adventofcode.com/2023/day/1/input
-	var calibration_codes, err1 = get_calibration_codes()
-	if err1 != nil {
-		fmt.Println("Error:", err1)
-		return
-	}
+	// var calibration_codes, err1 = get_calibration_codes()
+	// if err1 != nil {
+	// 	fmt.Println("Error:", err1)
+	// 	return
+	// }
 
-	// Convert each line to a number
-	var calibration_values []int = make([]int, len(calibration_codes))
-	for i, line := range calibration_codes {
-		var value, err2 = calibration_value(line)
-		if err2 != nil {
-			fmt.Println("Error:", err2)
-			return
-		} else {
-			calibration_values[i] = value
-		}
-	}
+	// // Convert each line to a number
+	// var calibration_values []int = make([]int, len(calibration_codes))
+	// for i, line := range calibration_codes {
+	// 	var value, err2 = calibration_value(line)
+	// 	if err2 != nil {
+	// 		fmt.Println("Error:", err2)
+	// 		return
+	// 	} else {
+	// 		calibration_values[i] = value
+	// 	}
+	// }
 
-	var sum int = 0
-	for _, value := range calibration_values {
-		sum += value
-	}
+	// var sum int = 0
+	// for _, value := range calibration_values {
+	// 	sum += value
+	// }
 
-	fmt.Println("Sum:", sum)
+	// fmt.Println("Sum:", sum)
 }
 
 func get_calibration_codes() ([]string, error) {
@@ -68,19 +77,24 @@ func calibration_value(line string) (int, error) {
 	return ((digits[0] * 10) + digits[1]), nil
 }
 
-func first_and_last_digit(line string) ([2]int, bool) {
-	var digits [2]int
+func first_and_last_digit(line string) ([]int, bool) {
+	var digits = make([]int, 0, 2)
 	var passed_digit bool = false
+	var sequential_letters string = ""
 
 	for _, runeValue := range line {
 		if unicode.IsDigit(runeValue) {
+			sequential_letters = ""
 			value := int_from_unicode(runeValue)
-			if passed_digit {
-				digits = [2]int{digits[0], value}
+			if len(digits) == 0 {
+				digits = append(digits, value)
+				digits = append(digits, value)
 			} else {
-				digits = [2]int{value, value}
+				digits[1] = value
 			}
 			passed_digit = true
+		} else {
+			sequential_letters += string(runeValue)
 		}
 	}
 
@@ -89,4 +103,14 @@ func first_and_last_digit(line string) ([2]int, bool) {
 
 func int_from_unicode(runeValue rune) int {
 	return int(runeValue - '0')
+}
+
+func test_calibration_value(code string, expected int) {
+	var result, _ = calibration_value(code)
+	if result != expected {
+		fmt.Println(fmt.Sprintf("Failure: expected %d, got %d", expected, result))
+		return
+	} else {
+		fmt.Println("Success")
+	}
 }
