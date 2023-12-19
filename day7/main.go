59238332d86c89dbfd71c5bb04fd7217f5cff086
Mark Janzer <mark.janzer@gmail.com>
2023-12-19 02:27:01 -0800

    Almost solved part 1 of day7, issues with bytes

--- day7/main.go
+++ day7/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"advent-of-code-2023/lib"
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -17,14 +18,15 @@ QQQJA 483`
 
 const DataFile string = "data.txt"
 
-var HIGH_CARD string = "high card"
-var ONE_PAIR string = "one pair"
-var TWO_PAIR string = "two pair"
-var THREE_OF_A_KIND string = "three of a kind"
-var FULL_HOUSE string = "full house"
-var FOUR_OF_A_KIND string = "four of a kind"
-var FIVE_OF_A_KIND string = "five of a kind"
+const HIGH_CARD string = "high card"
+const ONE_PAIR string = "one pair"
+const TWO_PAIR string = "two pair"
+const THREE_OF_A_KIND string = "three of a kind"
+const FULL_HOUSE string = "full house"
+const FOUR_OF_A_KIND string = "four of a kind"
+const FIVE_OF_A_KIND string = "five of a kind"
 
+// Can I use constants here?
 var TypeStrengths = map[string]int{
 	"high card":       0,
 	"one pair":        1,
@@ -77,15 +79,82 @@ func makeHand(line string) (hand Hand) {
 	parts := strings.Split(line, " ")
 	hand.Cards = parts[0]
 	hand.Bid, _ = strconv.Atoi(parts[1])
-	hand.Type = "dunno"
-	// hand.Type = determineType(hand)
+	hand.Type = determineType(hand)
 	return
+}
+
+func determineType(hand Hand) string {
+	cardMap := lib.FrequencyMap(strings.Split(hand.Cards, ""))
+	orderedCardCounts := []int{}
+	for _, count := range cardMap {
+		orderedCardCounts = append(orderedCardCounts, count)
+	}
+	// Sort in reverse order
+	sort.Slice(orderedCardCounts, func(i, j int) bool {
+		return orderedCardCounts[i] > orderedCardCounts[j]
+	})
+
+	if orderedCardCounts[0] == 1 {
+		return HIGH_CARD
+	} else if orderedCardCounts[0] == 2 {
+		if orderedCardCounts[1] == 2 {
+			return TWO_PAIR
+		} else {
+			return ONE_PAIR
+		}
+	} else if orderedCardCounts[0] == 3 {
+		if orderedCardCounts[1] == 2 {
+			return FULL_HOUSE
+		} else {
+			return THREE_OF_A_KIND
+		}
+	} else if orderedCardCounts[0] == 4 {
+		return FOUR_OF_A_KIND
+	} else if orderedCardCounts[0] == 5 {
+		return FIVE_OF_A_KIND
+	} else {
+		panic("Unknown hand type")
+	}
+}
+
+func orderHands(hands []Hand) []Hand {
+	sort.Slice(hands, func(i, j int) bool {
+		firstHand := hands[i]
+		secondHand := hands[j]
+		if TypeStrengths[firstHand.Type] < TypeStrengths[secondHand.Type] {
+			return true
+		} else if TypeStrengths[firstHand.Type] > TypeStrengths[secondHand.Type] {
+			return false
+		} else {
+			return compareCards(firstHand.Cards, secondHand.Cards)
+		}
+	})
+	return hands
+}
+
+func compareCards(firstCards, secondCards string) bool {
+	for i := 0; i < len(firstCards); i++ {
+		if CardStrengths[string(firstCards[i])] < CardStrengths[string(secondCards[i])] {
+			return true
+		} else if CardStrengths[string(firstCards[i])] > CardStrengths[string(secondCards[i])] {
+			return false
+		} else {
+			break
+		}
+	}
+	return false
 }
 
 func solvePart1(input string) int {
 	hands := makeHands(input)
-	fmt.Println(hands)
-	return 0
+	orderedHands := orderHands(hands)
+	totalWinnings := 0
+	for i := 0; i < len(orderedHands); i++ {
+		winnings := orderedHands[i].Bid * (i + 1)
+		fmt.Println(orderedHands[i], winnings)
+		totalWinnings += winnings
+	}
+	return totalWinnings
 }
 
 /*
