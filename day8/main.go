b532476dda3426a7d689b9ab6edfb6b2826af9f1
Mark Janzer <mark.janzer@gmail.com>
2023-12-19 16:24:58 -0800

    Progress on day 8 part 2. None of the journeys are resolvingK

--- day8/main.go
+++ day8/main.go
@@ -23,6 +23,17 @@ EEE = (EEE, EEE)
 GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)`
 
+const TestString2 string = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`
+
 const DataFile string = "data.txt"
 
 /*
@@ -79,24 +90,93 @@ func solvePart1(input string) int {
 /*
 	Part 2 Notes
 
+	Find all of the starting paths (every node that ends with A)
+	Change to be able to iterate while navigating
+
+
 */
 
+type Journey struct {
+	Start        string
+	Current      string
+	ResolvedOnce bool
+}
+
+func newJourney(key string) Journey {
+	return Journey{
+		Start:   key,
+		Current: key,
+	}
+}
+
+func (journey *Journey) atDestination() bool {
+	atDestination := string(journey.Current[2]) == "Z"
+	if atDestination && !journey.ResolvedOnce {
+		journey.ResolvedOnce = true
+	}
+	return atDestination
+}
+
+func (journey *Journey) travel(direction string, nodeMap NodeMap) {
+	newLocation := nodeMap[journey.Current][direction]
+	journey.Current = newLocation
+}
+
+type Journeys []Journey
+
+func (journeys Journeys) allAtDestination() bool {
+	for i := range journeys {
+		if !journeys[i].atDestination() {
+			return false
+		}
+	}
+	return true
+}
+
+func (journeys Journeys) travel(direction string, nodeMap NodeMap) {
+	for i := range journeys {
+		journeys[i].travel(direction, nodeMap)
+	}
+}
+
 func solvePart2(input string) int {
-	return 0
+	splitString := strings.Split(input, "\n\n")
+	inputString := strings.Split(splitString[0], "")
+	nodesString := splitString[1]
+	nodeMap := makeNodeMap(nodesString)
+
+	journeys := Journeys{}
+	for nodeKey := range nodeMap {
+		if string(nodeKey[2]) == "A" {
+			journeys = append(journeys, newJourney(nodeKey))
+		}
+	}
+
+	count := 0
+	for !journeys.allAtDestination() && count < len(inputString) {
+		inputIndex := count % len(inputString)
+		direction := inputString[inputIndex]
+		fmt.Println(direction)
+		journeys.travel(direction, nodeMap)
+		fmt.Println(journeys)
+		count++
+	}
+	fmt.Println(journeys)
+	return count
 }
 
 func main() {
 	lib.AssertEqual(2, solvePart1(TestString))
 	lib.AssertEqual(6, solvePart1(SmallTestString))
 
-	// lib.AssertEqual(1, solvePart2(TestString))
+	lib.AssertEqual(6, solvePart2(TestString2))
 	// lib.AssertEqual(1, solvePart2(SmallTestString))
 
-	dataString := lib.GetDataString(DataFile)
-	result1 := solvePart1(dataString)
-	fmt.Println(result1)
-
 	// dataString := lib.GetDataString(DataFile)
-	// result2 := solvePart2(dataString)
-	// fmt.Println(result2)
+	// result1 := solvePart1(dataString)
+	// fmt.Println(result1)
+
+	dataString := lib.GetDataString(DataFile)
+	result2 := solvePart2(dataString)
+	fmt.Println(result2)
 }
