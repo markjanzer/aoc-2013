4d3ad65048e274652f7c6e86a1d3536eeb7cd464
Mark Janzer <mark.janzer@gmail.com>
2024-01-17 16:57:15 -0800

    Solve day 13 part 2

--- day13/main.go
+++ day13/main.go
@@ -65,7 +65,6 @@ func solvePart1(input string) (sum int) {
 
 func solvePart1ForGrid(input string) int {
 	grid := lib.StringToGrid(input)
-	// return horizontalSymmetryAfterRow(grid)
 	return (100 * horizontalSymmetryAfterRow(grid)) + verticalSymmetryAfterColumn(grid)
 }
 
@@ -105,24 +104,139 @@ func horizontalSymmetryAfterRow(grid [][]byte) int {
 /*
 	Part 2 Notes
 
+	I need to know the original reflection line and track it.
+	Then we need to find another reflection line given that one of the characters is flipped
+
+	First let's just use arrays of strings rather than grids, string comparison should be easier and faster than
+	array comparison. Just tried it and apparently it's slower. Flipping took considerably longer. Comparison
+	was 4x faster with strings, but the difference was still just nanoseconds.
+
+	Then I can write a method that checks if the strings are off by one, and then we'll expect one of those
+	when checking for reflection.
 */
 
-func solvePart2(input string) int {
+// type SymmetryDirection string
+
+// const (
+// 	Vertical   SymmetryDirection = "Vertical"
+// 	Horizontal SymmetryDirection = "Horizontal"
+// )
+
+// type SymmetryInfo struct {
+// 	Direction SymmetryDirection
+// 	Index     int
+// }
+
+func solvePart2ForGrid(input string) int {
+	grid := lib.StringToGrid(input)
+
+	horizontalSymmetry := horizontalSymmetryAfterRow(grid)
+	verticalSymmetry := verticalSymmetryAfterColumn(grid)
+	alteredHorizontalSymmetry := 0
+	alteredVerticalSymmetry := 0
+
+	if horizontalSymmetry != 0 {
+		fmt.Println("horizontal")
+		alteredHorizontalSymmetry = alteredHorizontalSymmetryAfterRow(grid, horizontalSymmetry)
+		fmt.Println("vertical")
+		alteredVerticalSymmetry = alteredVerticalSymmetryAfterColumn(grid, -1)
+	} else if verticalSymmetry != 0 {
+		fmt.Println("horizontal")
+		alteredHorizontalSymmetry = alteredHorizontalSymmetryAfterRow(grid, -1)
+		fmt.Println("vertical")
+		alteredVerticalSymmetry = alteredVerticalSymmetryAfterColumn(grid, verticalSymmetry)
+	} else {
+		panic("No original symmetry found")
+	}
+
+	fmt.Println(horizontalSymmetry, verticalSymmetry, alteredHorizontalSymmetry, alteredVerticalSymmetry)
+
+	return (100 * alteredHorizontalSymmetry) + alteredVerticalSymmetry
+}
+
+func alteredVerticalSymmetryAfterColumn(grid [][]byte, originalSymmetry int) int {
+	flippedGrid := lib.FlipGrid(grid)
+	return alteredHorizontalSymmetryAfterRow(flippedGrid, originalSymmetry)
+}
+
+func alteredHorizontalSymmetryAfterRow(grid [][]byte, originalSymmetry int) int {
+	for i := 1; i < len(grid); i++ {
+		if i == originalSymmetry {
+			continue
+		}
+
+		// Continue if symmetry is not found
+		prev := i - 1
+		currentDifference := differenceBetweenSlices(grid[i], grid[prev])
+		fmt.Println(sliceOfBytesToStrings(grid[prev]))
+		fmt.Println(sliceOfBytesToStrings(grid[i]))
+		fmt.Println(currentDifference)
+		if currentDifference > 1 {
+			continue
+		}
+
+		// If symmetry is found, continue looking forward and
+		// back to compare rows until not possible
+		symmetrical := true
+		symmetryDistance := 1
+		for (i+symmetryDistance) < len(grid) && (prev-symmetryDistance) >= 0 {
+			currentDifference += differenceBetweenSlices(grid[i+symmetryDistance], grid[prev-symmetryDistance])
+			if currentDifference > 1 {
+				symmetrical = false
+				break
+			}
+			symmetryDistance++
+		}
+
+		if symmetrical && currentDifference == 1 {
+			return i
+		}
+	}
+
 	return 0
 }
 
+func differenceBetweenSlices(a, b []byte) int {
+	difference := 0
+	for i := range a {
+		if a[i] != b[i] {
+			difference++
+		}
+	}
+	return difference
+}
+
+func solvePart2(input string) (sum int) {
+	inputs := strings.Split(input, "\n\n")
+
+	for _, input := range inputs {
+		sum += solvePart2ForGrid(input)
+	}
+
+	return
+}
+
+func sliceOfBytesToStrings(input []byte) []string {
+	var output []string
+	for _, b := range input {
+		output = append(output, string(b))
+	}
+	return output
+}
+
 func main() {
-	lib.AssertEqual(5, solvePart1(SmallTestString))
-	lib.AssertEqual(405, solvePart1(TestString))
-	// lib.AssertEqual(1, solvePart2(TestString))
+	// lib.AssertEqual(5, solvePart1(SmallTestString))
+	// lib.AssertEqual(405, solvePart1(TestString))
+	// lib.AssertEqual(300, solvePart2(SmallTestString))
+	// lib.AssertEqual(400, solvePart2(TestString))
 
 	// lib.AssertEqual(1, solvePart2(SmallTestString))
 
-	dataString := lib.GetDataString(DataFile)
-	result1 := solvePart1(dataString)
-	fmt.Println(result1)
-
 	// dataString := lib.GetDataString(DataFile)
-	// result2 := solvePart2(dataString)
-	// fmt.Println(result2)
+	// result1 := solvePart1(dataString)
+	// fmt.Println(result1)
+
+	dataString := lib.GetDataString(DataFile)
+	result2 := solvePart2(dataString)
+	fmt.Println(result2)
 }
