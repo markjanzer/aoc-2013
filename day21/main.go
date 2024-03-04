b439a1ba458413e74e4260ff421b492961603e21
Mark Janzer <mark.janzer@gmail.com>
2024-03-04 15:31:42 -0800

    Attempt day 21 part 2

--- day21/main.go
+++ day21/main.go
@@ -130,22 +130,83 @@ func evenOrOdd(num int) string {
 
 */
 
-func solvePart2(input string) int {
-	return 0
+func canMove(position tile, grid [][]byte) bool {
+	rows := len(grid)
+	cols := len(grid[0])
+
+	gridRow := (rows + (position.row % rows)) % rows
+	gridCol := (cols + (position.col % cols)) % cols
+
+	if grid[gridRow][gridCol] != '#' {
+		fmt.Println(gridRow, gridCol)
+	}
+
+	return grid[gridRow][gridCol] != '#'
+}
+
+func moveAllDirections2(position tile, grid [][]byte, oddity string, tileCache *map[tile]string) (nextPosition []tile) {
+	var nextTile tile
+
+	allDirections := []tile{
+		{position.row - 1, position.col},
+		{position.row + 1, position.col},
+		{position.row, position.col - 1},
+		{position.row, position.col + 1},
+	}
+
+	for _, nextTile = range allDirections {
+		if canMove(nextTile, grid) {
+			if _, ok := (*tileCache)[nextTile]; !ok {
+				(*tileCache)[nextTile] = oddity
+				nextPosition = append(nextPosition, nextTile)
+			}
+		}
+	}
+
+	return
+}
+
+func solvePart2(input string, steps int) int {
+	grid := lib.StringToGrid(input)
+	startingTile := startingTile(grid)
+	tileCache := map[tile]string{}
+
+	queue := []tile{startingTile}
+	for i := 0; i < steps; i++ {
+		oddity := evenOrOdd(i + 1)
+		nextQueue := []tile{}
+		for _, position := range queue {
+			nextQueue = append(nextQueue, moveAllDirections2(position, grid, oddity, &tileCache)...)
+		}
+
+		queue = nextQueue
+	}
+
+	desiredOddity := evenOrOdd(steps)
+	count := 0
+	for _, oddity := range tileCache {
+		if oddity == desiredOddity {
+			count++
+		}
+	}
+	return count
 }
 
 func main() {
-	lib.AssertEqual(16, solvePart1(TestString, 6))
-	// lib.AssertEqual(1, solvePart2(TestString))
+	// fmt.Println(-4 % 3)
 
-	// lib.AssertEqual(1, solvePart1(SmallTestString))
-	// lib.AssertEqual(1, solvePart2(SmallTestString))
-
-	dataString := lib.GetDataString(DataFile)
-	result1 := solvePart1(dataString, 64)
-	fmt.Println(result1)
+	// lib.AssertEqual(16, solvePart1(TestString, 6))
+	// lib.AssertEqual(1594, solvePart2(TestString, 50))
+	lib.AssertEqual(6536, solvePart2(TestString, 100))
+	// lib.AssertEqual(167004, solvePart2(TestString, 500))
+	// lib.AssertEqual(668697, solvePart2(TestString, 1000))
+	// lib.AssertEqual(16733044, solvePart2(TestString, 5000))
 
 	// dataString := lib.GetDataString(DataFile)
-	// result2 := solvePart2(dataString)
+	// result1 := solvePart1(dataString, 64)
+	// fmt.Println(result1)
+
+	// dataString := lib.GetDataString(DataFile)
+	// result2 := solvePart2(dataString, 26501365)
 	// fmt.Println(result2)
 }
